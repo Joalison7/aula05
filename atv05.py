42bbf6a4f6c82a563926fe4fe41eae7991bee6d2
Joalison <joalisonalison970@gmail.com>
2025-04-15 16:52:41 -0300

    completo

--- atv05.py
+++ atv05.py
@@ -1,14 +1,19 @@
-n1 = float(input("sua primeira nota: "))
+resp = "s"
+while resp == "s":
+    n1 = float(input("sua primeira nota: "))
 
-while n1<0 or n1>10:
-    n1 = int(input("numero invalido digite novemente: "))
+    while n1<0 or n1>10:
 
-n2 = float(input("sua segunda nota: "))
+        n1 = int(input("numero invalido digite novemente: "))
 
-while n2<0 or n2>10:
-    n2 = int(input("numero invalido digite novemente: "))
+    n2 = float(input("sua segunda nota: "))
 
-calculo = (n1+n2)/2
+    while n2<0 or n2>10:
 
-print(calculo)
+        n2 = int(input("numero invalido digite novemente: "))
 
+    calculo = (n1+n2)/2
+
+    print(calculo)
+
+resp = input("deseja fazer novemente(s/n)")
